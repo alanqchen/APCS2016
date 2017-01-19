3e9c9374cbc94cd59d189ac9bd9f0126beb72231
nodinawe <alanchen1908@gmail.com>
2017-01-19 14:52:05 -0500

    Update StackListClient.java

--- Stack and Queue Assignment/StackListClient.java
+++ Stack and Queue Assignment/StackListClient.java
@@ -1,1 +1,28 @@
-
+import java.util.Scanner;
+public class StackListClient {
+	private static Scanner input = new Scanner(System.in); 
+	public static void main(String[] args) {
+		int menu=0;
+		do {
+			System.out.println("#======= Stack List Client =======#");
+			System.out.println("|1. Enter Sentence                |");
+			System.out.println("|2. Find a word and replace it    |");
+			System.out.println("|3. Display the sentence          |");
+			System.out.println("|4. McKeen's test case            |");
+			System.out.println("|5. Quit                          |");
+			System.out.println("#=================================#");
+			System.out.print("Please choose a selection: ");
+			menu = input.nextInt();
+				if (menu == 1) {
+          
+        } else if (menu == 2) {
+          
+        } else if (menu == 3) {
+          
+        } else if (menu == 4) {
+          
+        } else if (menu == 5) {
+          
+        }
+      }while (menu != 8);
+}
