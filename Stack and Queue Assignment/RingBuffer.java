a829e638fb2731e707191927c695cb16c65a56e3
nodinawe <alanchen1908@gmail.com>
2017-01-24 00:03:29 -0500

    Update RingBuffer.java

--- Stack and Queue Assignment/RingBuffer.java
+++ Stack and Queue Assignment/RingBuffer.java
@@ -18,17 +18,52 @@ public class RingBuffer {
 		rear = 0;
 	}
 	public boolean add(char ch) {
+		System.out.println(characters.length);
 		//should be a series of if loops, cases for wraped, non-wraped, empty, non-empty, and more.
-		if (rear - front == characters.length() - 1) {
+		if (rear - front == characters.length - 1) {
 			return false;
 		} else if (front - rear == 1) {
 			return false;
 		}
-		if (rear == characters.length() {
+		if (rear == characters.length - 1) {
 			rear = 0;
 		}
 		characters[rear] = ch;
 		rear++;
 		return true;
+	}
+	public char remove() {
+		if(isEmpty()) {
+			return ' ';
+		}
+		char temp = characters[front];
+		if(front == characters.length) {
+			front = 0;
+		} else {
+			front++;
+		}
+		return temp;
+	}
+	public char peek() {
+		if(isEmpty()) {
+			return '!';
+		}
+		return characters[front];
+	}
+	public int getFront() {
+		return front;
+	}
+	public int getRear() {
+		return rear;
+	}
+	public void display() {
+		String s1 = "";
+		String s2 = "";
+		for (char a : characters) {
+			s1 += String.valueOf(a) + " | ";
+		}
+		s2 += "Front: " + getFront() + "\nRear: " + getRear();
+		System.out.println(s1);
+		System.out.println(s2);
 	}
 }
