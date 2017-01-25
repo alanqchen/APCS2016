7da7fae9acb723bf2f183de52ba1c2fa30c0056c
nodinawe <alanchen1908@gmail.com>
2017-01-25 09:41:08 -0500

    Update RingBufferRunner.java

--- Stack and Queue Assignment/RingBufferRunner.java
+++ Stack and Queue Assignment/RingBufferRunner.java
@@ -2,6 +2,7 @@
 public class RingBufferRunner {
 	private static RingBuffer ring;
 	public static void main(String[] args) {
+		/*
 		ring = new RingBuffer(4);
 		ring.add('a');
 		ring.add('b');
@@ -11,6 +12,23 @@ public class RingBufferRunner {
 		ring.remove();
 		ring.display();
 		ring.add('e');
+		ring.display();
+		System.out.println(ring.add('f'));
+		ring.display();
+		ring.remove();
+		ring.remove();
+		ring.remove();
+		ring.remove();
+		ring.display();
+		*/
+		ring = new RingBuffer(1);
+		ring.add('a');
+		System.out.println(ring.add('f'));
+		ring.remove();
+		System.out.println(ring.add('b'));
+		System.out.println(ring.add('c'));
+		ring.remove();
+		System.out.println(ring.add('e'));
 		ring.display();
 	}
 
