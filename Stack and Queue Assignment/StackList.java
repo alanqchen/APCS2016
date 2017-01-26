481e78611676934e9501b152dd9a3b2530544fb3
nodinawe <alanchen1908@gmail.com>
2017-01-25 23:15:27 -0500

    Update StackList.java

--- Stack and Queue Assignment/StackList.java
+++ Stack and Queue Assignment/StackList.java
@@ -6,18 +6,23 @@
  * TODO: DONE
  */
 import java.util.LinkedList;
-
+//First in, Last out
 public class StackList<E> {
+	//list that stores the data
 	private LinkedList<E> list = new LinkedList<E>();
+	//adds data at the top of the stack
 	public void push(E x) {
 		list.addFirst(x);
 	}
+	//removes the data at the top of the stack
 	public <E> E pop() {
 		return (E) list.removeFirst();
 	}
+	//returns the data at the top of the list
 	public <E> E peek() {
 		return (E) list.get(0);
 	}
+	//returns if the stack is empty or not
 	public boolean isEmpty() {
 		if (list.size() == 0)
 			return true;
