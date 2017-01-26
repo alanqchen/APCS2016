5190e33e73c9c63e692ecc454a102b80584ac13b
nodinawe <alanchen1908@gmail.com>
2017-01-25 21:39:04 -0500

    Update QueueList.java

--- Stack and Queue Assignment/QueueList.java
+++ Stack and Queue Assignment/QueueList.java
@@ -5,19 +5,24 @@
  * TODO: DONE
  */
 import java.util.LinkedList;
-
+//First in, First out
 public class QueueList<E> {
+	//Linked List that stores the data
 	private LinkedList<E> list = new LinkedList<E>();
+	//Adds the data to the back
 	public boolean add (E x) {
 		list.addLast(x);
 		return true;
 	}
+	//Removes the data at the front
 	public <E> E remove() {
 		return (E) list.removeFirst();
 	}
+	//returns the data at the front
 	public <E> E peek() {
 		return (E) list.getFirst();
 	}
+	//returns if the queue is empty or not
 	public boolean isEmpty() {
 		if (list.size() == 0)
 			return true;
