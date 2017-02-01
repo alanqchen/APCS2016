50db7f6ff87a615c52f4e1565c3840575d4c3cae
nodinawe <alanchen1908@gmail.com>
2017-02-01 13:02:39 -0500

    Update FamilyMap.java

--- Family Map Assignment/FamilyMap.java
+++ Family Map Assignment/FamilyMap.java
@@ -1,1 +1,30 @@
-
+/**
+ * Alan Chen - APCompSci 2017
+ * Project: Linked Lists
+ * Date started: 2/1/2017
+ * BONUS: 
+ * TODO: 
+ */
+import java.util.*;
+public class FamilyMap {
+  private static Scanner input = new Scanner(System.in); 
+  private Map<String,TreeSet<String>> fm;
+  public FamilyMap() {
+    fm = new TreeMap<String,TreeSet<String>>();
+  }
+  public void addFamily() {
+    System.out.print("Please enter the last name: ");
+    String last = input.next();
+    String name = "filler";
+    int count = 1;
+    TreeSet<String> ns = new TreeSet<String>();
+    while(!name.equals("")) {
+      if (counter == 1) 
+        System.out.println("Enter nothing to stop.");
+      System.out.print("Please enter the first name #" + count + ": ");
+      name = input.next();
+      ns.add(name);
+      counter++;
+    }
+  }
+}
