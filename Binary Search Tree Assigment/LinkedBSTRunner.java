c8443daa1cdab4fab8fc2a6b08d35b21ef3169ab
nodinawe <alanchen1908@gmail.com>
2017-02-21 09:39:08 -0500

    Update LinkedBSTRunner.java

--- Binary Search Tree Assigment/LinkedBSTRunner.java
+++ Binary Search Tree Assigment/LinkedBSTRunner.java
@@ -1,1 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class LinkedBSTRunner {
+
+	public static void main(String[] args) {
+		List display = new ArrayList();
+		LinkedBST tree = new LinkedBST();
+		tree.addNode("C");
+		tree.addNode("A");
+		tree.addNode("D");
+		tree.addNode("E");
+		tree.addNode("B");
+		display = tree.preOrder();
+		System.out.println(display);
+	}
+
+}
