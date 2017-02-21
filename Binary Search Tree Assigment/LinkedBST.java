10307207d538d73adef0eed9892256674ce0716d
nodinawe <alanchen1908@gmail.com>
2017-02-21 09:39:28 -0500

    Update LinkedBST.java

--- Binary Search Tree Assigment/LinkedBST.java
+++ Binary Search Tree Assigment/LinkedBST.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Alan Chen - APCompSci 2017
  * Project: Linked Lists
@@ -7,6 +10,9 @@
  */
 public class LinkedBST {
 	private TreeNode root;
+	public LinkedBST() {
+		root = null;
+	}
 	public boolean addNode(Comparable obj) {
 		if (root == null) {
 			root = new TreeNode(obj);
@@ -17,14 +23,14 @@ public class LinkedBST {
 	private boolean add(Comparable obj, TreeNode node) {
 		if (obj == node.getValue())
 			return false;
-		if (obj.compareTo(node) < 0) {
+		if (obj.compareTo(node.getValue()) < 0) {
 			if (node.getLeft() == null) {
 				node.setLeft(new TreeNode(obj));
 				return true;
 			}
 			return add(obj, node.getLeft());
 		}
-		if (obj.compareTo(node) > 0) {
+		if (obj.compareTo(node.getValue()) > 0) {
 			if (node.getRight() == null) {
 				node.setRight(new TreeNode(obj));
 				return true;
@@ -32,5 +38,17 @@ public class LinkedBST {
 			return add(obj, node.getRight());
 		}
 		return false;
+	}
+	public List preOrder() {
+		List list1 = new ArrayList();
+		preOrderTraverse(root, list1);
+		return list1;
+	}
+	private void preOrderTraverse(TreeNode current, List list) {
+		if (current != null) {
+			list.add(current.getValue());
+			preOrderTraverse(current.getLeft(),list);
+			preOrderTraverse(current.getRight(),list);
+		}
 	}
 }
