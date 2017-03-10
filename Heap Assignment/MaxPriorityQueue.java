a2a8ef39c95b0602376fe45b44b0cafef8ee877b
nodinawe <alanchen1908@gmail.com>
2017-03-10 09:35:42 -0500

    Update MaxPriorityQueue.java

--- Heap Assignment/MaxPriorityQueue.java
+++ Heap Assignment/MaxPriorityQueue.java
@@ -1,1 +1,13 @@
+public interface MaxPriorityQueue
+{
+	boolean isEmpty();
+	boolean add(Comparable x);
+	Comparable removeMax();
+	Comparable peekMax();
+	
+	String toString( );		
+	boolean isFull( );
+	int getCount();
+ 
+}
 
