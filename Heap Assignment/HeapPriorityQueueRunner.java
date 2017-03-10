33faa126f23f7a1e854bc927eec67ce9d1f36fcd
nodinawe <alanchen1908@gmail.com>
2017-03-10 09:35:26 -0500

    Update HeapPriorityQueueRunner.java

--- Heap Assignment/HeapPriorityQueueRunner.java
+++ Heap Assignment/HeapPriorityQueueRunner.java
@@ -1,1 +1,27 @@
 
+public class HeapPriorityQueueRunner {
+	static HeapPriorityQueue main = new HeapPriorityQueue(5);
+	public static void main(String[] args) {
+		/*
+		 *           5
+		 *          / \
+		 *         4   3
+		 *        / \  
+		 *       3   4
+		 *      /
+		 *     2
+		 *    /    
+		 *   1    
+		 *     
+		 */
+		main.add(4);
+		main.add(6);
+		main.add(3);
+		main.add(5);
+		main.add(8);
+		main.add(10);
+		main.toStringBONUS();
+	}
+
+}
+
