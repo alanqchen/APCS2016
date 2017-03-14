2df825c4355cff1aeeb7e6db18acda87da1dded9
nodinawe <alanchen1908@gmail.com>
2017-03-13 23:56:11 -0400

    Update HeapPriorityQueueRunner.java

--- Heap Assignment/HeapPriorityQueueRunner.java
+++ Heap Assignment/HeapPriorityQueueRunner.java
@@ -2,25 +2,53 @@
 public class HeapPriorityQueueRunner {
 	static HeapPriorityQueue main = new HeapPriorityQueue(5);
 	public static void main(String[] args) {
-		/*
-		 *           5
-		 *          / \
-		 *         4   3
-		 *        / \  
-		 *       3   4
-		 *      /
-		 *     2
-		 *    /    
-		 *   1    
-		 *     
-		 */
+		System.out.println("COUNT: " + main.getCount());
 		main.add(4);
 		main.add(6);
 		main.add(3);
 		main.add(5);
 		main.add(8);
-		main.add(10);
-		main.toStringBONUS();
+		System.out.println(main.add(10));
+		System.out.println(main.isFull());
+		System.out.println(main.peekMax());
+		System.out.println("COUNT: " + main.getCount());
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.add(13);
+		System.out.println(main.toString());
+		main.add(2);
+		System.out.println(main.toString());
+		main.add(7);
+		System.out.println(main.toString());
+		main.add(18);
+		System.out.println(main.toString());
+		System.out.println(main.isFull());
+		System.out.println(main.peekMax());
+		System.out.println("COUNT: " + main.getCount());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		main.removeMax();
+		System.out.println(main.toString());
+		System.out.println(main.peekMax());
+		System.out.println("COUNT: " + main.getCount());
+		System.out.println(main.isFull());
 	}
 
 }
